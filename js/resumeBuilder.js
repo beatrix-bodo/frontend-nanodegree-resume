54dca3a462cac16c655f86c8d986d12218eb974f
beatrix-bodo <beatrix.bodo@gmail.com>
2015-11-06 00:35:12 +0100

    Add name and role to header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,21 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var email = "hello@trixi.graphics"
+var newEmail =
+    email.replace("hello@trixi.graphics", "beatrix.bodo@gmail.com");
+    console.log(email);
+    console.log(newEmail);
+
+var awesomeThoughts = "My name is Trixi and I am AWESOME!";
+    console.log(awesomeThoughts);
+var funThoughts =
+    awesomeThoughts.replace("AWESOME", "FUN")
+$("#main").append(funThoughts);
+
+var name = "Beatrix Bodó"
+var formattedName =
+    HTMLheaderName.replace("%data%", name)
+$("#header").append(formattedName);
+
+var role = "graphic designer"
+var formattedRole =
+HTMLheaderRole.replace("%data%", role)
+$("#header").append(formattedRole);
